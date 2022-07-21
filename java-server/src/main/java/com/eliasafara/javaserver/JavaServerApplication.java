7002c7a363e2a6d93f9208a5dc0fef17064b0b40
Elias Afara <afaraelias@gmail.com>
2022-07-21 22:48:37 +0200

    Java backend setup with spring intializer

---
+++ java-server/src/main/java/com/eliasafara/javaserver/JavaServerApplication.java
@@ -0,0 +1,13 @@
+package com.eliasafara.javaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServerApplication.class, args);
+	}
+
+}
---
+++ java-server/src/test/java/com/eliasafara/javaserver/JavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliasafara.javaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

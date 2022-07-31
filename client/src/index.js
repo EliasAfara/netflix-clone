98200ecd561be55fdc5b16e4a3cd74c86c8219e0
Elias Afara <afaraelias@gmail.com>
2022-07-31 22:28:40 +0200

    Applicaiton index files

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3001';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

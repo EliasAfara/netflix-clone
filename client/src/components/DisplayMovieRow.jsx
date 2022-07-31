fe077bbaa11e582959f3141d8c25d2dbd7f71b5d
Elias Afara <afaraelias@gmail.com>
2022-08-01 00:18:04 +0200

    Removed unused stylings

--- client/src/components/DisplayMovieRow.jsx
+++ client/src/components/DisplayMovieRow.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
-import styled from 'styled-components';
 
 import { useViewport } from '../hooks/useViewport';
 import { IoPlayCircleSharp } from 'react-icons/io5';
@@ -124,82 +122,5 @@ const DisplayMovieRow = ({ title, movies, isLiked = false }) => {
     </>
   );
 };
-
-const Container = styled.div`
-  max-width: 230px;
-  width: 230px;
-  height: 100%;
-  cursor: pointer;
-  position: relative;
-  img {
-    border-radius: 0.2rem;
-    width: 100%;
-    height: 100%;
-    z-index: 10;
-  }
-  .hover {
-    z-index: 99;
-    height: max-content;
-    width: 20rem;
-    position: absolute;
-    top: -18vh;
-    left: 0;
-    border-radius: 0.3rem;
-    box-shadow: rgba(0, 0, 0, 0.75) 0px 3px 10px;
-    background-color: #181818;
-    transition: 0.3s ease-in-out;
-    .image-video-container {
-      position: relative;
-      height: 140px;
-      img {
-        width: 100%;
-        height: 140px;
-        object-fit: cover;
-        border-radius: 0.3rem;
-        top: 0;
-        z-index: 4;
-        position: absolute;
-      }
-      video {
-        width: 100%;
-        height: 140px;
-        object-fit: cover;
-        border-radius: 0.3rem;
-        top: 0;
-        z-index: 5;
-        position: absolute;
-      }
-    }
-    .info-container {
-      padding: 1rem;
-      gap: 0.5rem;
-    }
-    .icons {
-      .controls {
-        display: flex;
-        gap: 1rem;
-      }
-      svg {
-        font-size: 2rem;
-        cursor: pointer;
-        transition: 0.3s ease-in-out;
-        &:hover {
-          color: #b8b8b8;
-        }
-      }
-    }
-    .genres {
-      ul {
-        gap: 1rem;
-        li {
-          padding-right: 0.7rem;
-          &:first-of-type {
-            list-style-type: none;
-          }
-        }
-      }
-    }
-  }
-`;
 
 export default DisplayMovieRow;

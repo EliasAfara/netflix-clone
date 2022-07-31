eaf7f1d7f396938c2cfdd622ebab360eec0bef66
Elias Afara <afaraelias@gmail.com>
2022-07-31 22:27:36 +0200

    Header, Footer, Navbar design ready

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <AppFooter className='appFooter'>
+      <div className='footer__copyright'>
+        &copy; 2022 Made with ❤️ by{' '}
+        <a
+          className='footer__copyright--link'
+          href='https://github.com/EliasAfara'
+        >
+          {' '}
+          Elias Afara
+        </a>
+      </div>
+    </AppFooter>
+  );
+};
+
+const AppFooter = styled.footer`
+  padding-top: 35px;
+  padding-bottom: 15px;
+  grid-column: 1 / 13;
+  text-align: center;
+
+  .footer__copyright {
+    color: #fff;
+    font-size: 16px;
+
+    .footer__copyright--link {
+      text-decoration: none;
+      color: $color-red-2;
+      transition: all 0.3s;
+    }
+
+    .footer__copyright--link:hover {
+      color: orange;
+    }
+  }
+`;
+
+export default Footer;

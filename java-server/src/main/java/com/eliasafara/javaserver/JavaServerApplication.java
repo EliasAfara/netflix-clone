e9995f1496cf68cf7d3ea18863df53f50fac292c
Elias Afara <afaraelias@gmail.com>
2022-08-11 16:32:19 +0300

    Modified Java Application Server

--- java-server/src/main/java/com/eliasafara/javaserver/JavaServerApplication.java
+++ java-server/src/main/java/com/eliasafara/javaserver/JavaServerApplication.java
@@ -2,10 +2,10 @@ package com.eliasafara.javaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class JavaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JavaServerApplication.class, args);
 	}

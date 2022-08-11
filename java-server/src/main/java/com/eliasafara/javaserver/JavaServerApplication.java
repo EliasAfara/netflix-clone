116ddce1bb316c31f486a76ec18e130c34fe00ee
Elias Afara <afaraelias@gmail.com>
2022-08-11 19:36:23 +0300

    implemented login & registration

--- java-server/src/main/java/com/eliasafara/javaserver/JavaServerApplication.java
+++ java-server/src/main/java/com/eliasafara/javaserver/JavaServerApplication.java
@@ -2,9 +2,8 @@ package com.eliasafara.javaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class JavaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaServerApplication.class, args);
---
+++ java-server/src/main/java/com/eliasafara/javaserver/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.eliasafara.javaserver.controller;
+
+import com.eliasafara.javaserver.datamodel.Contact;
+import com.eliasafara.javaserver.datamodel.User;
+import com.eliasafara.javaserver.repository.ContactRepository;
+import com.eliasafara.javaserver.repository.RoleRepository;
+import com.eliasafara.javaserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User req) {
+        try {
+            System.out.println(req.getContact());
+            Contact contact = contactRepository.save(req.getContact());
+            User new_user = userRepository.save(new User(
+                    req.getUsername(),
+                    req.getPassword(),
+                    req.getRole(),
+                    contact)
+            );
+            return new ResponseEntity<>(new_user, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User req) {
+        try {
+            List<User> foundUsers = new ArrayList<>(userRepository.findByUsernameAndPassword(req.getUsername(), req.getPassword()));
+            if (foundUsers.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            User loggedInUser = new User(
+                    foundUsers.get(0).getUsername(),
+                    foundUsers.get(0).getRole(),
+                    foundUsers.get(0).getContact()
+            );
+            return new ResponseEntity<>(loggedInUser, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}

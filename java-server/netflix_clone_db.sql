ae837e49c963c2247027dfe7df6adb95b1567014
Elias Afara <afaraelias@gmail.com>
2022-08-11 16:31:55 +0300

    Modified database queries

--- java-server/netflix_clone_db.sql
+++ java-server/netflix_clone_db.sql
@@ -6,18 +6,29 @@ CREATE DATABASE netflixclonedb with template=template0 owner=netflixcloneuser;
 alter default privileges grant all on tables to netflixcloneuser;
 
 CREATE TABLE role(
-                     role_id serial PRIMARY KEY,
-                     role_name varchar(10) NOT NULL
+                     role_name varchar(10) NOT NULL,
+                     PRIMARY KEY (role_name)
 );
 
 INSERT INTO role (role_name) VALUES ('admin');
 INSERT INTO role (role_name) VALUES ('user');
 
+CREATE TABLE contacts(
+                         contact_id serial PRIMARY KEY,
+                         first_name varchar(255),
+                         last_name varchar(355),
+                         date_of_birth DATE,
+                         gender varchar(6),
+                         contact_email varchar(150) NOT NULL,
+                         UNIQUE(contact_email)
+);
+
 CREATE TABLE users(
                       user_id serial PRIMARY KEY,
                       username varchar(45) UNIQUE,
                       password text NOT NULL,
-                      role_id int REFERENCES role (role_id)
+                      "role" varchar(10) not null REFERENCES role (role_name),
+                      contact_id int not null REFERENCES contacts (contact_id)
 );
 
 CREATE TABLE movies(
@@ -32,17 +43,6 @@ CREATE TABLE seenMovie(
                           user_id int REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
                           movie_id int REFERENCES movies (movie_id) ON UPDATE CASCADE ON DELETE CASCADE,
                           seenMovie_date DATE NOT NULL
-);
-
-CREATE TABLE contacts(
-                         contact_id serial PRIMARY KEY,
-                         first_name varchar(255),
-                         last_name varchar(355),
-                         date_of_birth DATE,
-                         gender varchar(6),
-                         contact_email varchar(150) NOT NULL,
-                         user_id int REFERENCES users (user_id),
-                         UNIQUE(contact_email)
 );
 
 CREATE TABLE address(

10105ac9b676cf4b7341a23f958c7ce0fec8f6e6
Elias Afara <afaraelias@gmail.com>
2022-08-12 03:07:25 +0300

    side helper functions

--- node-server/themoviedb/insertMovies.js
+++ node-server/themoviedb/insertMovies.js
@@ -3,22 +3,51 @@ const axios = require('axios');
 
 // Inserting movies into the database from the JSON file
 
-movies.forEach((movie) => {
-  const config = {
-    method: 'post',
-    url: 'http://localhost:3001/api/movies/addMovie',
-    headers: {
-      'Content-Type': 'application/json',
-      Cookie: 'JSESSIONID=E67B7317F7D106F8B7E91E168E9EF3BF',
-    },
-    data: movie,
-  };
+// movies.forEach((movie) => {
+//   const config = {
+//     method: 'post',
+//     url: 'http://localhost:3001/api/movies/addMovie',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Cookie: 'JSESSIONID=E67B7317F7D106F8B7E91E168E9EF3BF',
+//     },
+//     data: movie,
+//   };
 
-  axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
+//   axios(config)
+//     .then(function (response) {
+//       console.log(JSON.stringify(response.data));
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// });
+
+// reset movie ratings
+axios({
+  method: 'get',
+  url: 'http://localhost:3001/api/movies/',
+  headers: {},
+})
+  .then(function (response) {
+    let movies = response.data.movies;
+    movies.forEach((movie) => {
+      console.log(movie._id);
+
+      axios({
+        method: 'put',
+        url: `http://localhost:3001/api/movies/rating/updateMovieAverageRating/${movie._id}`,
+        headers: {},
+        data: '',
+      })
+        .then(function (response) {
+          console.log(response.data.averageRating);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     });
-});
+  })
+  .catch(function (error) {
+    console.log(error);
+  });

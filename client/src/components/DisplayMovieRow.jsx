f4c8c614d761ff835315a88595c61e7f4bff4d2c
Elias Afara <afaraelias@gmail.com>
2022-08-12 10:48:06 +0300

    displaying movie average rating & stars

--- client/src/components/DisplayMovieRow.jsx
+++ client/src/components/DisplayMovieRow.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,8 +10,13 @@ import { AiOutlinePlus } from 'react-icons/ai';
 import { RiThumbUpFill, RiThumbDownFill } from 'react-icons/ri';
 import { BiChevronDown } from 'react-icons/bi';
 import { BsCheck } from 'react-icons/bs';
+import { SiImdb } from 'react-icons/si';
 
 import './styles/movieShowcase.scss';
+import ReactStars from 'react-rating-stars-component';
+
+// State Context
+import useAppStateContext from '../hooks/useAppStateContext';
 
 // install Swiper components
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
@@ -21,6 +26,19 @@ const DisplayMovieRow = ({ title, movies, isLiked = false }) => {
   const [windowDimensions] = useViewport();
   const { width } = windowDimensions;
   const navigate = useNavigate();
+
+  const { appState } = useAppStateContext();
+
+  const [movieRating, setMovieRating] = useState({
+    rating: 0,
+    comment: 'I like this movie',
+    userId: appState.user_id,
+  }); // initial rating value
+
+  // Catch Rating value
+  const handleRating = (rate) => {
+    setMovieRating({ ...movieRating, rating: rate });
+  };
 
   return (
     <>
@@ -106,6 +124,20 @@ const DisplayMovieRow = ({ title, movies, isLiked = false }) => {
                   <div className='info'>
                     <BiChevronDown title='More Info' />
                   </div>
+                </div>
+                <div className='ratings flex j-between a-center'>
+                  <div>
+                    &nbsp;
+                    <SiImdb title='IMDB' /> &nbsp;&nbsp;
+                    {movieData.averageRating.toFixed(2)}
+                  </div>
+                  <ReactStars
+                    count={5}
+                    value={movieData.averageRating}
+                    onChange={handleRating}
+                    size={24}
+                    activeColor='#ffd700'
+                  />
                 </div>
                 <div className='genres flex'>
                   <ul className='flex'>
